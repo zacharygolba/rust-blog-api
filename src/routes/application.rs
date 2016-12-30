afa4d2b2ff04132d650195dc8836ff845ace97be
Zachary Golba <zachary.golba@postlight.com>
2016-12-30 12:58:20 -0500

    test: add test for health check

--- src/routes/application.rs
+++ src/routes/application.rs
@@ -9,3 +9,22 @@ pub fn health() -> JSON<HashMap<&'static str, &'static str>> {
         "detail" => "OK",
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use rocket;
+    use rocket::http::Status;
+    use rocket::http::Method::*;
+    use rocket::testing::MockRequest;
+
+    use super::*;
+
+    #[test]
+    fn test_health() {
+        let server = rocket::ignite().mount("/", routes![health]);
+        let mut req = MockRequest::new(Get, "/health");
+        let res = req.dispatch_with(&server);
+
+        assert_eq!(res.status(), Status::Ok);
+    }
+}
