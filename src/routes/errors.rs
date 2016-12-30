90b52191db657fb3b8a76f09ff963393db84587d
Zachary Golba <zachary.golba@postlight.com>
2016-12-30 12:55:45 -0500

    test: add tests for error catchers

--- src/routes/errors.rs
+++ src/routes/errors.rs
@@ -39,3 +39,66 @@ pub fn internal_server_error() -> JSON<ErrorMap> {
         ],
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use rocket;
+    use rocket::http::Status;
+    use rocket::http::Method::*;
+    use rocket::testing::MockRequest;
+
+    use super::*;
+
+    #[derive(Deserialize, Serialize)]
+    struct Params {
+        message: String,
+    }
+
+    #[patch("/echo", data = "<params>", format="application/json")]
+    fn echo(params: JSON<Params>) -> JSON<Params> {
+        params
+    }
+
+    #[get("/error")]
+    fn error() -> Result<(), ()> {
+        Err(())
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_request() {
+        let server = rocket::ignite()
+            .mount("/", routes![echo])
+            .catch(errors![bad_request]);
+
+        let mut req = MockRequest::new(Patch, "/echo");
+        let res = req.dispatch_with(&server);
+
+        assert_eq!(res.status(), Status::BadRequest);
+    }
+
+    #[test]
+    fn test_not_found() {
+        let server = rocket::ignite()
+            .mount("/", routes![])
+            .catch(errors![not_found]);
+
+        let mut req = MockRequest::new(Get, "/");
+        let res = req.dispatch_with(&server);
+
+        assert_eq!(res.status(), Status::NotFound);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_error() {
+        let server = rocket::ignite()
+            .mount("/", routes![error])
+            .catch(errors![internal_server_error]);
+
+        let mut req = MockRequest::new(Get, "/error");
+        let res = req.dispatch_with(&server);
+
+        assert_eq!(res.status(), Status::InternalServerError);
+    }
+}
